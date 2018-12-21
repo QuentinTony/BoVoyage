29f75aef33c35bfcf6d667cc624010af59642c8b
inti0490 <inti0490@192.168.1.168>
2018-12-21 12:22:09 +0100

    Ingrid: voyage controller est fini sauf pour les images

--- BoVoyage/src/main/java/fr/adaming/controllers/VoyageController.java
+++ BoVoyage/src/main/java/fr/adaming/controllers/VoyageController.java
@@ -1,5 +1,146 @@
 package fr.adaming.controllers;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import fr.adaming.model.Voyage;
+import fr.adaming.service.IVoyageService;
+
+@Controller
+@RequestMapping("/bovoyage")
 public class VoyageController {
-kugyftr
+
+	@Autowired
+	private IVoyageService voService;
+
+	public void setVoService(IVoyageService voService) {
+		this.voService = voService;
+	}
+
+	//1********************************************LIST*********************************************************************
+	
+	@RequestMapping(value="/listvoyage", method=RequestMethod.GET)
+	public ModelAndView afficheListe() {
+		//Récuperer la liste de la BD
+		
+		List<Voyage> listVoyage= voService.getAllVoyage();
+		
+		return new ModelAndView("accueil","listVoyage",listVoyage);
+		
+	}
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		DateFormat formatDate= new SimpleDateFormat("yyyy-MM-dd");
+		formatDate.setLenient(false);
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(formatDate, false));
+	}
+	
+	//2****************************************ADD*******************************************************
+	
+		@RequestMapping(value="/addvoyage", method=RequestMethod.GET)
+		public String getAdd(Model modele) {
+			
+			modele.addAttribute("voyage", new Voyage());
+			return "addvoyage";
+		}
+		
+		@RequestMapping(value="/addvoyagep", method=RequestMethod.POST)
+		public String submitAdd(@ModelAttribute("voyage") Voyage vo , RedirectAttributes ra) {
+			
+		
+			Voyage vOut= voService.addVoyage(vo);
+		
+			
+			if(vOut.getId()!=0) {
+				
+				
+				return "redirect:listvoyage";
+			}else {
+				ra.addAttribute("msg", "L'ajout n'est pas fait");
+				return "redirect:addvoyage";
+			}
+		}
+		
+		//3*************************************************UPDATE***************************************************************
+		
+		@RequestMapping(value="/updatevoyage", method=RequestMethod.GET)
+		public String updateVoyage(Model model) {
+			model.addAttribute("voyage", new Voyage());
+			return "updatevoyage";
+		}
+		
+		@RequestMapping(value="/updatevoyagep", method=RequestMethod.POST)
+		public String submitUpdate(@ModelAttribute("voyage") Voyage vo, RedirectAttributes ra) {
+			
+		int verif= voService.updateVoyage(vo);
+	if(verif!=0) {
+				
+				//rediriger ver la methode /liste
+				return "redirect:listvoyage";
+			}else {
+				ra.addAttribute("msg", "La modification n'est pas faite");
+				return "redirect:updatevoyage";
+			}
+		}
+	
+		
+		//4*************************************************DELETE***************************************************************
+		
+
+		@RequestMapping(value="/deletevoyage", method=RequestMethod.GET)
+		public String deleteVoyage(Model model) {
+			model.addAttribute("voyage", new Voyage());
+			return "deletevoyage";
+		}
+		
+		@RequestMapping(value="/deletevoyagep", method=RequestMethod.POST)
+		public String submitdelete(@ModelAttribute("voyage") Voyage vo, RedirectAttributes ra) {
+			
+		int verif= voService.deleteVoyage(vo);
+	if(verif!=0) {
+				
+				//rediriger ver la methode /liste
+				return "redirect:listvoyage";
+			}else {
+				ra.addAttribute("msg", "Le voyage n'est pas effacé");
+				return "redirect:deletevoyage";
+			}
+		}
+		
+		//5*************************************************GET***************************************************************
+		
+		@RequestMapping(value="/getvoyage", method=RequestMethod.GET)
+		public String getVoyage(Model model) {
+			model.addAttribute("voyage", new Voyage());
+			return "getvoyage";
+		}
+		
+		@RequestMapping(value="/getvoyagep", method=RequestMethod.POST)
+		public String submitgetvoyage(@ModelAttribute("voyage") Voyage vo, RedirectAttributes ra) {
+			
+		Voyage vOut= voService.getVoyage(vo.getId());
+	if(vOut.getId()!=0) {
+				
+				//rediriger ver la methode /liste
+				return "redirect:listvoyage";
+			}else {
+				ra.addAttribute("msg", "Le voyage n'existe pas");
+				return "redirect:getvoyage";
+			}
+		}
 }
