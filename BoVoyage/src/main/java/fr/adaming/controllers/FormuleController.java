83fdcf2b6b1c1e4c58f553a96d8d5470e1893031
INTI0489 <INTI0489@192.168.1.56>
2019-01-03 16:25:57 +0100

    ronan: formuleController

--- BoVoyage/src/main/java/fr/adaming/controllers/FormuleController.java
+++ BoVoyage/src/main/java/fr/adaming/controllers/FormuleController.java
@@ -26,6 +26,7 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.jsf.FacesContextUtils;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import fr.adaming.model.Banque;
 import fr.adaming.model.Client;
 import fr.adaming.model.Destination;
 import fr.adaming.model.Formule;
@@ -37,6 +38,7 @@ import fr.adaming.model.Prestation;
 import fr.adaming.model.Vehicule;
 import fr.adaming.model.Voyage;
 import fr.adaming.service.IAssuranceService;
+import fr.adaming.service.IBanqueService;
 import fr.adaming.service.IClientService;
 import fr.adaming.service.IDestinationService;
 import fr.adaming.service.IFormuleService;
@@ -52,8 +54,8 @@ import fr.adaming.service.VoyageServiceImpl;
 @RequestMapping("/formule")
 public class FormuleController {
 
-	public static final String dest= "C:\\Users\\inti0490\\Desktop\\Formation\\Workspace\\GenerationPDF\\essaye1";
-	
+	public static final String dest = "C:\\Users\\inti0490\\Desktop\\Formation\\Workspace\\GenerationPDF\\essaye1";
+
 	@Autowired
 	private IVoyageService voService;
 	@Autowired
@@ -72,6 +74,13 @@ public class FormuleController {
 	private IClientService clService;
 	@Autowired
 	private IPrestationService prService;
+
+	@Autowired
+	private IBanqueService baService;
+
+	public void setBaService(IBanqueService baService) {
+		this.baService = baService;
+	}
 
 	public void setClService(IClientService clService) {
 		this.clService = clService;
@@ -108,23 +117,21 @@ public class FormuleController {
 	public void setFoService(IFormuleService foService) {
 		this.foService = foService;
 	}
-	
+
 	@InitBinder
 	public void initBinder(WebDataBinder binder) {
 		DateFormat formatDate = new SimpleDateFormat("yyyy-MM-dd");
 		formatDate.setLenient(false);
 		binder.registerCustomEditor(Date.class, new CustomDateEditor(formatDate, false));
 	}
-	
 
 	@RequestMapping(value = "/selectformule", method = RequestMethod.GET)
 	public String selectvoyage(Model model, @RequestParam("id") long id, HttpServletRequest serreq) {
-	
-		
+
 		Formule fo = new Formule();
-		
+
 		fo.setClient((Client) serreq.getSession(false).getAttribute("client"));
-		
+
 		model.addAttribute("formule", fo);
 
 		Voyage vOut = voService.getVoyage(id);
@@ -143,122 +150,137 @@ public class FormuleController {
 	}
 
 	@RequestMapping(value = "/selectformulep", method = RequestMethod.POST)
-	public String selectandsetVoyage(@ModelAttribute("formule") Formule fo, RedirectAttributes ra, HttpServletRequest serreq) {
+	public String selectandsetVoyage(@ModelAttribute("formule") Formule fo, RedirectAttributes ra,
+			HttpServletRequest serreq) {
 
-		Formule fOut = foService.addFormule(fo);
-		ra.addAttribute("id", fOut.getId());
-		Formule fIn = foService.getFormule(fOut.getId());
-		Date da = fIn.getVoyage().getDateDepart();
-		Date dr = fIn.getVoyage().getDateRetour();
-		final long MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24;
-		int delta = (int) ((dr.getTime() - da.getTime()) / MILLISECONDS_PER_DAY);
-		fIn.setNombreJour(delta);
-		fIn.setClient((Client) serreq.getSession(false).getAttribute("client"));
-		fIn.setNombreNuit(delta - 1);
-		foService.updateFormule(fIn);
-		Formule fIn2=foService.getFormule(fIn.getId());
-		serreq.getSession(false).setAttribute("formule", fIn2);
-		System.out.println(fIn2);
-		
-		if (fOut.getId() != 0) {
+		int a = fo.getVoyage().getStockPassager();
+		if (fo.getNombrePersonne() <= a) {
+			Formule fOut = foService.addFormule(fo);
+			ra.addAttribute("id", fOut.getId());
+			Formule fIn = foService.getFormule(fOut.getId());
+			Date da = fIn.getVoyage().getDateDepart();
+			Date dr = fIn.getVoyage().getDateRetour();
+			final long MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24;
+			int delta = (int) ((dr.getTime() - da.getTime()) / MILLISECONDS_PER_DAY);
+			fIn.setNombreJour(delta);
+			fIn.setClient((Client) serreq.getSession(false).getAttribute("client"));
+			fIn.setNombreNuit(delta - 1);
+			foService.updateFormule(fIn);
+			Formule fIn2 = foService.getFormule(fIn.getId());
+			serreq.getSession(false).setAttribute("formule", fIn2);
+			System.out.println(fIn2);
 
-			return "redirect:enregistrerpassager";
+			if (fOut.getId() != 0) {
+
+				return "redirect:enregistrerpassager";
+			} else {
+				ra.addAttribute("msg", "L'ajout n'est pas fait");
+				return "redirect:selectformule?id=" + fOut.getVoyage().getId();
+			}
 		} else {
-			ra.addAttribute("msg", "L'ajout n'est pas fait");
-			return "redirect:selectformule?id="+fOut.getId();
+			ra.addAttribute("msg", "Le nombre de passager rensseigné est trop élevé");
+			return "redirect:selectformule?id=" + fo.getVoyage().getId();
 		}
 
 	}
 
-	
-
-	
 	@RequestMapping(value = "/enregistrerpassager", method = RequestMethod.GET)
 	public String getAdd(Model modele) {
 
 		modele.addAttribute("passager", new Passager());
-		
+
 		return "addpassager";
 	}
-	
-	
+
 	@RequestMapping(value = "/enregistrerpassagerp", method = RequestMethod.POST)
-	public String enregistrerpassagerp(RedirectAttributes ra, @Valid @ModelAttribute("passager") Passager pa, BindingResult br, HttpServletRequest serreq) {
-		
+	public String enregistrerpassagerp(RedirectAttributes ra, @Valid @ModelAttribute("passager") Passager pa,
+			BindingResult br, HttpServletRequest serreq) {
+
 		if (br.hasErrors()) {
 
 			return "addpassager";
 		}
-		
+
 		Passager paa = paService.addPassager(pa);
-		
+
 		if (paa.getId() != 0) {
-			
-				Client cl = (Client) serreq.getSession(false).getAttribute("client");
-				paa.setClient(cl);
-				paService.updatePassager(paa);
-		
-				Formule foIn=(Formule) serreq.getSession(false).getAttribute("formule");
-				System.out.println(foIn);
-				foIn=foService.getFormule(foIn.getId());
-				Voyage vo=foIn.getVoyage();
-				vo.setStockPassager(vo.getStockPassager() - 1);
-				voService.updateVoyage(vo);
-				
-				
-				int compteur=foIn.getCompteur();
-				compteur=compteur+1;
-				foIn.setCompteur(compteur);
-				foService.updateFormule(foIn);
-				
-				if(compteur<foIn.getNombrePersonne()) {
-					System.out.println(compteur);
-					
-					return "redirect:enregistrerpassager";
-				}
-					
-					return "redirect:validformule/"+foIn.getId();
-				
+
+			Client cl = (Client) serreq.getSession(false).getAttribute("client");
+			paa.setClient(cl);
+			paService.updatePassager(paa);
+
+			Formule foIn = (Formule) serreq.getSession(false).getAttribute("formule");
+			System.out.println(foIn);
+			foIn = foService.getFormule(foIn.getId());
+			Voyage vo = foIn.getVoyage();
+			vo.setStockPassager(vo.getStockPassager() - 1);
+			voService.updateVoyage(vo);
+
+			int compteur = foIn.getCompteur();
+			compteur = compteur + 1;
+			foIn.setCompteur(compteur);
+			foService.updateFormule(foIn);
+
+			if (compteur < foIn.getNombrePersonne()) {
+				System.out.println(compteur);
+
+				return "redirect:enregistrerpassager";
+			}
+
+			return "redirect:validformule/" + foIn.getId();
+
 		} else {
 			ra.addAttribute("msg", "L'enregistrement a échoué");
 			return "redirect:enregistrerpassager";
 		}
-		
+
 	}
-	
-	
 
 	@RequestMapping(value = "/validformule/{id}", method = RequestMethod.GET)
 	public String validvoyage(Model model, @PathVariable("id") long id) {
-		
+
 		Formule fIn = foService.getFormule(id);
-		
+
 		model.addAttribute("formule", fIn);
 
 		return "recapitulatifPanier";
 	}
-	
 
-	
 	@RequestMapping(value = "/validformule/validformulep", method = RequestMethod.POST)
 	public String validvoyagepost(@ModelAttribute("formule") Formule fo, RedirectAttributes ra) {
-		
-		MailConfirmation mail= new MailConfirmation();
-		PdfFactureVoyage pdf= new PdfFactureVoyage();
+
+		MailConfirmation mail = new MailConfirmation();
+		PdfFactureVoyage pdf = new PdfFactureVoyage();
 		try {
 			pdf.generarPdf(dest);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		Client cl= fo.getClient();
+		Client cl = fo.getClient();
 		mail.sendMailToCl(fo, cl);
 		foService.updateFormule(fo);
 
 		return "accueil";
 
 	}
-	
 
+	@RequestMapping(value = "/validformule/submitVirement", method = RequestMethod.POST)
+	public String submitVirement(RedirectAttributes ra, Model model, @ModelAttribute("compte") Banque b,
+			@RequestParam("dSomme") Double somme, @RequestParam("id") long id, HttpSession maSession) {
+		System.out.println("je lance la méthode viremente du controleur client");
+		Banque bBoVoyage = baService.virement(b, somme);
+		if (bBoVoyage.getId() != 0) {
+			maSession.setAttribute("indice", "block");
+			maSession.setAttribute("indice1", "none");
+			return "redirect:/formule/validformule/" + id;
+		} else {
+
+			ra.addAttribute("msg", "le virement a échoué");
+
+			return "redirect:/formule/validformule/" + id;
+		}
+
+	}
 
 }
